8d0893e72115886531fbf27526d8f53c1397f508
JafarAbdi <cafer.abdi@gmail.com>
2022-03-06 20:23:14 +0300

    Rename to open_quickfix

--- lua/cmake/utils.lua
+++ lua/cmake/utils.lua
@@ -42,8 +42,8 @@ end
 
 function utils.run(cmd, args, opts)
   vim.fn.setqflist({}, ' ', { title = cmd .. ' ' .. table.concat(args, ' ') })
-  opts.quickfix_only_on_error = vim.F.if_nil(opts.quickfix_only_on_error, config.quickfix_only_on_error)
-  if not opts.quickfix_only_on_error then
+  opts.open_quickfix = vim.F.if_nil(opts.open_quickfix, not config.quickfix_only_on_error)
+  if opts.open_quickfix then
     show_quickfix()
   end
 
@@ -59,7 +59,7 @@ function utils.run(cmd, args, opts)
         if opts.on_success then
           opts.on_success()
         end
-      elseif opts.quickfix_only_on_error then
+      elseif config.quickfix_only_on_error then
         show_quickfix()
         vim.api.nvim_command('cbottom')
       end
