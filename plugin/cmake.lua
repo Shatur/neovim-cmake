38b7dd93015330206e2a88a6a1aaebad4c35ed94
Hennadii Chernyshchyk <genaloner@gmail.com>
2022-04-16 19:22:09 +0300

    Add command description

--- plugin/cmake.lua
+++ plugin/cmake.lua
@@ -4,4 +4,4 @@ if not vim.fn.has('nvim-0.7.0') then
 end
 
 local cmake_commands = require('cmake.commands')
-vim.api.nvim_create_user_command('CMake', cmake_commands.run_command, { nargs = '*', complete = cmake_commands.match_commands })
+vim.api.nvim_create_user_command('CMake', cmake_commands.run_command, { nargs = '*', complete = cmake_commands.match_commands, desc = 'Run CMake command' })
